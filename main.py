9ab9936e2eed5be4a41c47f6a6a47f3f8e304249
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2023-01-14 11:44:53 -0800

    now main.py can take arguments from command line
    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
+import argparse
 import csv
 import os
 from typing import Any, Optional
 
 from user import User
 
-inputDir: str = "public-test-data"
-outputDir: str = "output"
+# using argparse to parse the argument from command line
+parser: argparse.ArgumentParser = argparse.ArgumentParser()
+parser.add_argument("-i", help="input folder")
+parser.add_argument("-o", help="output folder")
+args: argparse.Namespace = parser.parse_args()
+
+# obtain input and output directory from command line
+inputDir: str = args.i
+outputDir: str = args.o
 
 # check to see if out folder folder exists
 if not os.path.exists(outputDir):
     os.mkdir(outputDir)
 
-# database for storing user information, key is user id
-database: dict[str, User] = {}
 # all the information types
 keys: Optional[tuple[str, ...]] = None
 
+# read the csv file
 with open(os.path.join(inputDir, "profile", "profile.csv"), newline="") as f:
-    # read the csv file
     spamreader = csv.reader(f, delimiter=" ", quotechar="|")
+
+    # process data from csv file
     for row in spamreader:
         _temp: list[str] = row[0].split(",")
         if keys is not None:
             userInfo: dict[str, Any] = {}
-            assert len(keys) == len(_temp)
+            # assert len(keys) == len(_temp)
             for i, _value in enumerate(keys):
                 userInfo[_value] = _temp[i]
             User(
